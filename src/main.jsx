206b152afb284d5645584d8232a88e190a931acb
jaisriv788 <srivastavajai845@gmail.com>
2025-10-16 18:10:06 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/u2u_global_react/">
+    <BrowserRouter basename="/u2u_global_react/app/">
       <App />
     </BrowserRouter>
   </StrictMode>
